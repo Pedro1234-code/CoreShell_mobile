bc7261cf0ad087dfc455b94b63281bb918d6139d
winux <78425126+Pedro1234-code@users.noreply.github.com>
2024-06-11 13:10:33 -0300

    Added shortcut for File Explorer

---
+++ ExplorerHook/Program.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+
+namespace ExplorerHook
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe", "/c explorer.exe");
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
