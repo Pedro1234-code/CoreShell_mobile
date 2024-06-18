43fb17ebce2bcc5713e39294a9496c51f24a2aa6
winux <78425126+Pedro1234-code@users.noreply.github.com>
2024-06-18 19:53:35 -0300

    Centered things

--- MobileOS Update/MainPage.xaml.cs
+++ MobileOS Update/MainPage.xaml.cs
@@ -26,5 +26,15 @@ namespace MobileOS_Update
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
